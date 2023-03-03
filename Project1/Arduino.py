95ec6d7acd37a46b738b4060916ad5e0fce13a9a
Patricia Marques <mpatricia2002@gmail.com>
2023-03-03 15:23:15 +0000

    buttons

--- Project1/Arduino.py
+++ Project1/Arduino.py
@@ -2,3 +2,29 @@
 # analógico de uma das suas entradas analógicas
 # Se o comando não for o correcto envia uma mensagem de erro ao Raspberry Pi
 # Se o comando for o correcto envia o valor adquirido de volta ao Raspberry Pi por USB
+
+USB_PORT = "/dev/ttyACM0"
+
+void setup() // runs once when the sketch starts
+{
+  // make the LED pin (pin 13) an output pin
+  pinMode(ledPin, OUTPUT);
+
+  // initialize serial communication
+  Serial.begin(9600);
+  while(!Serial){}
+}
+
+void loop() // runs repeatedly after setup() finishes
+{
+  sensorValue = analogRead(sensorPin);  // read pin A0   
+  Serial.println(sensorValue);         // send data to serial
+
+  if (sensorValue < 500) {            // less than 500?
+    digitalWrite(ledPin, LOW); }     // Turn the LED off
+
+  else {                               // greater than 500?
+    digitalWrite(ledPin, HIGH); }     // Keep the LED on
+
+  delay(100);             // Pause 100 milliseconds
+}
