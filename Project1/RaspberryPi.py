73e0f8e74692d567fd6d5457f5ab16038c6b0173
Patricia Marques <mpatricia2002@gmail.com>
2023-03-03 15:16:37 +0000

    buttons

--- Project1/RaspberryPi.py
+++ Project1/RaspberryPi.py
@@ -12,6 +12,10 @@ import os
 import serial
 import numpy as np
 import sys
+import serial
+import time
+
+#FRONTEND
 
 class MainWindow(QtWidgets.QMainWindow):
 
@@ -68,9 +72,7 @@ if __name__ == '__main__':
     main()
 
 
-# Envia, a intervalos definidos, um comando ao Arduíno
-# para que este adquira um valor analógico e o envie
-# Imprime esse valor, e actualiza o plot
+#BACKEND
 
 # USB_PORT = "/dev/ttyUSB0"  # Arduino Uno R3 Compatible
 USB_PORT = "/dev/ttyACM0"  # Arduino Uno WiFi Rev2
@@ -78,48 +80,17 @@ USB_PORT = "/dev/ttyACM0"  # Arduino Uno WiFi Rev2
 # Imports
 import serial
 
-# Functions
-def print_commands():
-   """Prints available commands."""
-   print("Available commands:")
-   print("  a - Retrieve Arduino value")
-   print("  l - Turn on Arduino LED")
-   print("  k - Turn off Arduino LED")
-   print("  x - Exit program")
-
-# Main
-# Connect to USB serial port at 9600 baud
 try:
    usb = serial.Serial(USB_PORT, 9600, timeout=2)
 except:
    print("ERROR - Could not open USB serial port.  Please check your port name and permissions.")
    print("Exiting program.")
    exit()
+
 # Send commands to Arduino
 print("Enter a command from the keyboard to send to the Arduino.")
-print_commands()
-while True:
-   command = input("Enter command: ")
-   if command == "a":  # read Arduino A0 pin value
-      usb.write(b'read_a0')  # send command to Arduino
-      line = usb.readline()  # read input from Arduino
-      line = line.decode()  # convert type from bytes to string
-      line = line.strip()  # strip extra whitespace characters
-      if line.isdigit():  # check if line contains only digits
-         value = int(line)  # convert type from string to int
-      else:
-         print("Unknown value '" + line + "', setting to 0.")
-         value = 0
-      print("Arduino A0 value:", value)
-   elif command == "l":  # turn on Arduino LED
-      usb.write(b'led_on')  # send command to Arduino
-      print("Arduino LED turned on.")
-   elif command == "k":  # turn off Arduino LED
-      usb.write(b'led_off')  # send command to Arduino
-      print("Arduino LED turned off.")
-   elif command == "x":  # exit program
-      print("Exiting program.")
-      exit()
-   else:  # unknown command
-      print("Unknown command '" + command + "'.")
-      print_commands()
+#while True:
+      #usb.write(b'read_a0')  # send command to Arduino
+      #line = usb.readline()  # read input from Arduino
+      #line = line.decode()  # convert type from bytes to string
+      #line = line.strip()  # strip extra whitespace characters
